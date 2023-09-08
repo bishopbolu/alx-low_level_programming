13b6c465c324e9784bd6c758a0401fc7fae609cd
bishopbolu <paulbolu178@gmail.com>
2023-09-08 10:40:19 -0700

    C Hello World

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 
 /**
- * main - Entry point
+ * main - a C program that prints exactly with proper
+ * grammar, but the outcome is a piece of art,
  *
  * Return - 0(Success)
  */
